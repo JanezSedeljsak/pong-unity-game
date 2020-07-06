83a8c90cfeb335ee6d3bb71955a3b84d20786f1d
PRO-BIT\janezs <janez.sedeljsak@gmail.com>
2020-07-06 15:03:13 +0200

    ball physics added

--- Assets/Player2Controlller.cs
+++ Assets/Player2Controlller.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1Controller : MonoBehaviour
+public class Player2Controlller : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,12 +13,11 @@ public class Player1Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.UpArrow)) {
             transform.Translate(Vector3.up * Time.deltaTime * 4f, Space.World);
         }
-        else if (Input.GetKey(KeyCode.S)) {
+        else if (Input.GetKey(KeyCode.DownArrow)) {
             transform.Translate(Vector3.down * Time.deltaTime * 4f, Space.World);
         }
-        
     }
 }
